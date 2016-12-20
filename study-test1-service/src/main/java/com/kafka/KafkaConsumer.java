0149f39f8289b370a6dffdccdb9f6e3c6b66e6b0
evefost <450463362@qq.com>
2016-12-20 18:27:26 +0800

    测试kafka

--- study-test1-service/src/main/java/com/kafka/KafkaConsumer.java
+++ study-test1-service/src/main/java/com/kafka/KafkaConsumer.java
@@ -1,12 +1,16 @@
 package com.kafka;
 
+import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.serializer.StringDecoder;
 import kafka.utils.VerifiableProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,48 +20,70 @@ import java.util.Properties;
  * Created by Germmy on 2016/7/10.
  */
 public class KafkaConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+    private static Properties props = new Properties();
+    private static ConsumerConnector consumer;
 
+    public final static String TOPIC = "TEST-TOPIC";
 
-    private  final ConsumerConnector consumer;
+    static {
+        try {
+            InputStream e = Thread.currentThread().getContextClassLoader().getResourceAsStream("properties/consumer.properties");
+            if (e == null) {
+                logger.error("启动加载消息消费都失败，找不到加载文件！！！！");
+            } else {
+                props.load(e);
+                boolean on = Boolean.valueOf(props.getProperty("consumer.on-off", "true")).booleanValue();
+                if (on) {
+                    ConsumerConfig config = new ConsumerConfig(props);
+                    long begin = System.currentTimeMillis();
+                    consumer = Consumer.createJavaConsumerConnector(config);
+                    logger.debug("connect zookeeper time:" + (System.currentTimeMillis() - begin) / 1000L + "s");
+                }
+            }
+        } catch (Exception var5) {
+            var5.printStackTrace();
+            logger.error("启动加载消息消费者失败！！！！", var5);
+        }
 
-    public final static String TOPIC="TEST-TOPIC";
+    }
 
-    private KafkaConsumer(){
-        Properties props=new Properties();
-        props.put("zookeeper.connect","192.168.134.129:2181");
-        props.put("group.id","jd-group");//消费组是什么概念？
+    private KafkaConsumer() {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", "192.168.134.129:2181");
+        props.put("group.id", "jd-group");//消费组是什么概念？
 
-        props.put("zookeeper.session.timeout.ms","60000");
-        props.put("zookeeper.sync.time.ms","200");
-        props.put("auto.commit.interval.ms","1000");
-        props.put("auto.offset.reset","smallest");
+        props.put("zookeeper.session.timeout.ms", "60000");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "smallest");
 
-        props.put("serializer.class","kafka.serializer.StringEncoder");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
 
-        ConsumerConfig config=new ConsumerConfig(props);
+        ConsumerConfig config = new ConsumerConfig(props);
 
-        consumer=kafka.consumer.Consumer.createJavaConsumerConnector(config);
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(config);
     }
 
 
-    void consume(){
-        Map<String,Integer> topicCountMap=new HashMap<String, Integer>();
-        topicCountMap.put(KafkaConsumer.TOPIC,new Integer(1));
-        StringDecoder keyDecoder=new StringDecoder(new VerifiableProperties());
-        StringDecoder valueDecoder=new StringDecoder(new VerifiableProperties());
+    void consume() {
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(KafkaConsumer.TOPIC, new Integer(1));
+        StringDecoder keyDecoder = new StringDecoder(new VerifiableProperties());
+        StringDecoder valueDecoder = new StringDecoder(new VerifiableProperties());
 
-        Map<String,List<KafkaStream<String,String>>> consumerMap=
-                consumer.createMessageStreams(topicCountMap,keyDecoder,valueDecoder);
+        Map<String, List<KafkaStream<String, String>>> consumerMap =
+                consumer.createMessageStreams(topicCountMap, keyDecoder, valueDecoder);
 
-        KafkaStream<String,String> stream=consumerMap.get(KafkaConsumer.TOPIC).get(0);
-        ConsumerIterator<String,String> it=stream.iterator();
-        while(it.hasNext()){
+        KafkaStream<String, String> stream = consumerMap.get(KafkaConsumer.TOPIC).get(0);
+        ConsumerIterator<String, String> it = stream.iterator();
+        while (it.hasNext()) {
             System.out.println(it.next().message());
         }
     }
 
 
-    public  static  void main(String[] args){
+    public static void main(String[] args) {
         new KafkaConsumer().consume();
     }
 
--- study-test1-service/src/main/java/com/kafka/KafkaProducer.java
+++ study-test1-service/src/main/java/com/kafka/KafkaProducer.java
@@ -1,9 +1,14 @@
 package com.kafka;
 
+import com.big.data.service.impl.MessageServiceImpl;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -11,31 +16,59 @@ import java.util.Properties;
  */
 public class KafkaProducer {
 
-    private  final Producer<String,String> producer;
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+    private static Properties props = new Properties();
+    private  static Producer<String,Object> producer;
 
     public final static String TOPIC="TEST-TOPIC";
+    static {
+        try {
+            InputStream e = Thread.currentThread().getContextClassLoader().getResourceAsStream("properties/producer.properties");
+            if(e == null) {
+                logger.error("启动加载消息生产者失败，找不到加载文件！！！！");
+            } else {
+                props.load(e);
+                boolean on = Boolean.valueOf(props.getProperty("producer.on-off", "true")).booleanValue();
+                if(on) {
+                    ProducerConfig config = new ProducerConfig(props);
+                    producer = new Producer(config);
+                }
+            }
+        } catch (IOException var3) {
+            logger.error("启动加载消息生产者失败！！！！");
+        }
+    }
+
+    public static void sendTopic(String key, Object value) {
+        if(null == producer) {
+            logger.warn("producer can\'t be null!");
+        } else {
+            KeyedMessage keyedMessage = new KeyedMessage(key, value);
+            producer.send(keyedMessage);
+        }
+    }
 
     private  KafkaProducer(){
-        Properties props=new Properties();
-        props.put("metadata.broker.list","192.168.134.129:9092");
-        props.put("serializer.class","kafka.serializer.StringEncoder");
-        props.put("key.serializer.class","kafka.serializer.StringEncoder");
-        props.put("request.required.acks","-1");
-        props.put("producer.type","async");
-        producer=new Producer<String, String>(new ProducerConfig(props)) ;
+//        Properties props=new Properties();
+//        props.put("metadata.broker.list","192.168.134.129:9092");
+//        props.put("serializer.class","kafka.serializer.StringEncoder");
+//        props.put("key.serializer.class","kafka.serializer.StringEncoder");
+//        props.put("request.required.acks","-1");
+//        props.put("producer.type","async");
+//        producer=new Producer<String, String>(new ProducerConfig(props)) ;
     }
 
 
     void produce(){
-        int messageNo=1000;
-        final int COUNT=10000;
-        while(messageNo<COUNT){
-            String key=String.valueOf(messageNo);
-            String data="hello kafka message"+key;
-            producer.send(new KeyedMessage<String,String>(TOPIC,key,data));
-            System.out.println(data);
-            messageNo++;
-        }
+//        int messageNo=1000;
+//        final int COUNT=10000;
+//        while(messageNo<COUNT){
+//            String key=String.valueOf(messageNo);
+//            String data="hello kafka message"+key;
+//            producer.send(new KeyedMessage<String,String>(TOPIC,key,data));
+//            System.out.println(data);
+//            messageNo++;
+//        }
     }
 
     public  static  void main(String[] args){
